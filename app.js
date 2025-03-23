b943e00572222959feabbe5d31348f525fbd49cc
pataegonia <clue9986@gmail.com>
2025-03-24 08:50:56 +0900

    add cors and myspl

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require(cors);
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 // 기본 라우터 예시
