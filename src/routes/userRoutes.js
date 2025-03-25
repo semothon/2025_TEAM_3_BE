18aa41fe150884183e21ca2f45b10167fc91b3e8
pataegonia <clue9986@gmail.com>
2025-03-25 18:20:10 +0900

    exporting login module

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-router.post('/api/login', userController.loginUser);
+router.post('/login', userController.loginUser);
 
 module.exports = router;
