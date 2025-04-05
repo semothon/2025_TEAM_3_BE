73b706c2b42f93ef1fea72c22034ed70b20f4057
pataegonia <clue9986@gmail.com>
2025-04-05 18:52:09 +0900

    ordering search results

--- src/routes/searchRoutes.js
+++ src/routes/searchRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const searchController = require('../controllers/searchController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
-router.get('/search', searchController.Search);
+router.get('/search',authMiddleware, searchController.Search);
 
 module.exports = router;
